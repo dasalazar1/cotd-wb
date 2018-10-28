c91f9892f441cf5ffb74de9eaa1a2f533a42fe9c
Daniel Salazar <dasalazar1@gmail.com>
2018-10-28 16:03:14 -0500

    Adding fish to order page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -27,13 +28,29 @@ class App extends React.Component {
     });
   };
 
+  addToOrder = key => {
+    // Take a copy of state
+    const order = { ...this.state.order };
+    // add or increment fish
+    order[key] = order[key] + 1 || 1;
+    //setsState
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market!" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}>
+                {key}
+              </Fish>
+            ))}
+          </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
