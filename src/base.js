4d8b8141d9a231b4e0e3bb4b0df101bcd6b16c9d
Daniel Salazar <dasalazar1@gmail.com>
2018-10-29 21:16:12 -0500

    firebase working

--- src/base.js
+++ src/base.js
@@ -1,2 +1,19 @@
 import Rebase from 're-base';
-import firebase from 'firebase';
+import * as firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyDXC6y2xO9pKXc_Iei1ODZH0oxNvEBLBS4',
+  authDomain: 'catch-of-the-day-d4155.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-d4155.firebaseio.com',
+  projectId: 'catch-of-the-day-d4155',
+  storageBucket: 'catch-of-the-day-d4155.appspot.com',
+  messagingSenderId: '26587727999'
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// This is a named export
+export { firebaseApp };
+
+// this is a default export
+export default base;
